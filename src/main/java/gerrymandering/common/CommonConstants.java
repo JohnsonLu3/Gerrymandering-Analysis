832796387f9dad88adb9e36f67875cc19ad1dc0a
ysuo85 <yi.suo@stonybrook.edu>
2017-12-02 17:18:27 -0500

    Implements getArea for Lat/Lng coordinate systems.

--- src/main/java/gerrymandering/common/CommonConstants.java
+++ src/main/java/gerrymandering/common/CommonConstants.java
@@ -11,4 +11,7 @@ public class CommonConstants {
     public static Double TTEST_THRESHOLD = 0.01;
     public static Double EFFICIENCYGAP_THRESHOLD = 0.08;
     public static Long UNCONTESTED = -1L;
+    public static Double EARTH_RADIUS_MI = 3959.00;
+    public static Double FULL_CIRCLE = 360.00;
+    public static Double EARTH_DEGREE_LENGTH = EARTH_RADIUS_MI * 2 * Math.PI / FULL_CIRCLE;
 }
