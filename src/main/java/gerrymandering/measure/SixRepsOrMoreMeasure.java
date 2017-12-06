a85bbe1570a3bb9e052194d9c19c26197940855e
JohnsonLu3 <JohnsonLu94@gmail.com>
2017-12-05 19:52:35 -0500

    worked on pieces of six reps or more measuer

--- src/main/java/gerrymandering/measure/SixRepsOrMoreMeasure.java
+++ src/main/java/gerrymandering/measure/SixRepsOrMoreMeasure.java
@@ -8,16 +8,37 @@ import gerrymandering.model.SuperDistrict;
  * Created by yisuo on 11/7/17.
  */
 public class SixRepsOrMoreMeasure implements Measure {
-    private Boolean checkNumDistricts(SuperDistrict s){
-        return false;
+    private int checkNumDistricts(SuperDistrict s){
+        int districtCount = s.getDistricts().size();
+
+        if(districtCount >= 6){
+            return -1;              // skipped
+        }
+
+        return 0;                   // continue
     }
 
-    private Boolean checkNumOfSuperDistricts(State state){
-        return false;
+    private int checkNumOfSuperDistricts(State state){
+
+        // Check the number of superdistricts plus the number of normal districts
+        // this should be less than the number of total representatives.
+        if((state.getSuperDistricts().size() + state.getDistricts().size()) < state.getDistricts().size()){
+            return -1;               // failed
+        }
+        // Check the number of non-super districts. If it is 0 then return a resultobject
+        // with the status as "failed" else continue
+        if(state.getDistricts().size() == 0){
+            return  -1;             // failed
+        }
+
+        return 0;                   // continue
     }
 
-    private Boolean checkNumOfRepsPerSuperDistrict(SuperDistrict s){
-        return false;
+    private int checkNumOfRepsPerSuperDistrict(SuperDistrict s){
+        // Check the number of representatives for each super distrist if it is greater
+        // than 3 and less than 5. If the check fails then return a resultobject with the
+        // status "failed" else "success".
+        return 0;                   // pass
     }
 
     @Override
