f314ccd6295892f65c9caa809e9b71e98a60259e
ysuo85 <yi.suo@stonybrook.edu>
2017-12-06 20:27:26 -0500

    Uncomments the Spring security related classes.

--- src/main/java/gerrymandering/bean/BCryptEncoder.java
+++ src/main/java/gerrymandering/bean/BCryptEncoder.java
@@ -1,12 +1,12 @@
-//package gerrymandering.bean;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//public class BCryptEncoder {
-//    @Bean(name="bCryptPasswordEncoder")
-//    public PasswordEncoder bCryptPasswordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package gerrymandering.bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class BCryptEncoder {
+    @Bean(name="bCryptPasswordEncoder")
+    public PasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
